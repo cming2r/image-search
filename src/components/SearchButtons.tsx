d33281e39c05c328f13ec4c14ea0297bc1779333
cming2r <cming2ring@gmail.com>
2025-03-23 22:06:28 +0800

    k

--- src/components/SearchButtons.tsx
+++ src/components/SearchButtons.tsx
@@ -2,6 +2,7 @@
 
 import { FC, useState, ReactElement } from 'react';
 import { saveSearchRecord, getDeviceType } from '@/lib/supabase';
+import Image from 'next/image';
 
 interface SearchButtonProps {
   imageUrl: string;
@@ -66,12 +67,16 @@ const SearchButtons: FC<SearchButtonProps> = ({ imageUrl }) => {
       bgColor: 'bg-blue-500',
       hoverColor: 'hover:bg-blue-600',
       icon: (
-        <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#FFFFFF" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
-          <path fill="#FFFFFF" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
-          <path fill="#FFFFFF" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
-          <path fill="#FFFFFF" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
-        </svg>
+        <div className="w-5 h-5 mr-2 flex items-center justify-center overflow-hidden">
+          <Image 
+            src="/images/google.svg" 
+            alt="Google" 
+            width={20} 
+            height={20} 
+            className="filter brightness-0 invert" 
+            priority
+          />
+        </div>
       )
     },
     {
@@ -80,9 +85,16 @@ const SearchButtons: FC<SearchButtonProps> = ({ imageUrl }) => {
       bgColor: 'bg-teal-500',
       hoverColor: 'hover:bg-teal-600',
       icon: (
-        <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#FFFFFF" d="M5.71 2h4.76L22 11.8v.2l-8.04 3.71-8.25-3.71V2zm0 0M5.71 2L4 3.03v11.94l7 3.85 7.98-3.85.02-1L5.71 2z"/>
-        </svg>
+        <div className="w-5 h-5 mr-2 flex items-center justify-center overflow-hidden">
+          <Image 
+            src="/images/bing.svg" 
+            alt="Bing" 
+            width={14} 
+            height={20} 
+            className="filter brightness-0 invert" 
+            priority
+          />
+        </div>
       )
     },
     {
@@ -91,9 +103,16 @@ const SearchButtons: FC<SearchButtonProps> = ({ imageUrl }) => {
       bgColor: 'bg-purple-500',
       hoverColor: 'hover:bg-purple-600',
       icon: (
-        <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#FFFFFF" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5zm9 0c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
-        </svg>
+        <div className="w-5 h-5 mr-2 flex items-center justify-center overflow-hidden">
+          <Image 
+            src="/images/tineye.png" 
+            alt="TinEye" 
+            width={20} 
+            height={20} 
+            className="filter brightness-0 invert" 
+            priority
+          />
+        </div>
       )
     },
     {
@@ -102,9 +121,16 @@ const SearchButtons: FC<SearchButtonProps> = ({ imageUrl }) => {
       bgColor: 'bg-indigo-500',
       hoverColor: 'hover:bg-indigo-600',
       icon: (
-        <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#FFFFFF" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88C7.55 15.8 9.68 15 12 15s4.45.8 6.14 2.12C16.43 19.18 14.03 20 12 20z"/>
-        </svg>
+        <div className="w-5 h-5 mr-2 flex items-center justify-center overflow-hidden">
+          <Image 
+            src="/images/SauceNAO.png" 
+            alt="SauceNAO" 
+            width={20} 
+            height={20} 
+            className="filter brightness-0 invert" 
+            priority
+          />
+        </div>
       )
     }
   ];
